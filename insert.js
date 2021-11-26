09e438b71e3c9d1e5125d52ddcee91ec531c5d71
ComboLeak <95103477+ComboLeak@users.noreply.github.com>
2021-11-26 21:53:43 +0100

    Update insert.js

--- insert.js
+++ insert.js
@@ -1,4 +1,4 @@
-liste = ["mail1", "mail2", "mail3", "mail4"]
+liste = ["kamilpro@interia.eu:1973kamil", "oliorz2004@interia.pl:monika35", "pawelstrusz@interia.pl:bucuskot22", "pawelstrusz@interia.pl:zuziaela15"]
 
 for(let i=0; i<liste.length; i++){
     document.getElementById("container").innerHTML += "<span>" + liste[i] + "</span><br><br>"
